f0a2b553ed113b0ccbe32c2e2b4b06b5d307eaff
prakasitz <59050083@kmitl.ac.th>
2023-10-05 16:56:25 +0700

    connect mongodb + nestjs env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { RegsiterMongooseConnection } from './connections/register.mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRootAsync({
+            useClass: RegsiterMongooseConnection,
+            connectionName: "RegisterMongoose",
+        }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/connections/register.mongoose.ts
@@ -0,0 +1,12 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { MongooseOptionsFactory, MongooseModuleOptions } from "@nestjs/mongoose";
+
+@Injectable()
+export class RegsiterMongooseConnection implements MongooseOptionsFactory {
+    createMongooseOptions(): MongooseModuleOptions {
+        return {
+            uri: 'mongodb://'+process.env.DB_HOST+':'+Number(process.env.DB_PORT)+'/',
+            dbName: process.env.DB_NAME, 
+        };
+    }
+}
